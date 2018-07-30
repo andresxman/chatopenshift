d6a75b899213ac3be7969732e58c7d08b0750ec3
andresxman <41528431+andresxman@users.noreply.github.com>
2018-07-30 04:24:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ var port1 = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
    res.send('Hello World');
-})
+});
 
 var server = app.listen(port1, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
-})
+});
